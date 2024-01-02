61b5fc5bdc14824cd858dc418087c5fb98a2656f
HShokaku <qiu3187336@gmail.com>
2024-01-02 08:00:25 +0000

    delete unnecessary files/code

--- src/utils/mol/dataset.py
+++ src/utils/mol/dataset.py
@@ -1,17 +1,5 @@
-from typing import Dict, List, Optional, Union
-from collections import OrderedDict
-from torch.utils.data import Dataset
-from rdkit import Chem
-from random import Random
-from src.utils.mol.feature_generator import get_features_generator
-from src.utils.mol.feature_graph import MolGraph, BatchMolGraph
-from src.utils import StandardScaler
-import numpy as np
-
 CACHE_GRAPH = False
 CACHE_MOL = False
-SMILES_TO_GRAPH: Dict[str, MolGraph] = {}
-SMILES_TO_MOL: Dict[str, Chem.Mol] = {}
 
 def cache_graph() -> bool:
     return CACHE_GRAPH
@@ -28,5 +16,4 @@ def set_cache_mol(cache_mol: bool) -> None:
     CACHE_MOL = cache_mol
 
 def empty_cache():
-    SMILES_TO_GRAPH.clear()
-    SMILES_TO_MOL.clear()
+    pass
